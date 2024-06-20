10d9b7fe355f521cc73e3d957ab5e0dc4a9ef243
Eren Yalçın <yalcineren97@gmail.com>
2024-06-21 00:59:45 +0300

    feat(board): implement a special shuffle function

--- src/board/boardUtils.ts
+++ src/board/boardUtils.ts
@@ -10,4 +10,93 @@ function getRandomColor() {
   return [Math.random() * 255, Math.random() * 255, Math.random() * 255];
 }
 
-export { divideInt, swap, getRandomColor };
+function slidePuzzleShuffle(array: any[], numberOfCols: number) {
+  /* This shuffle must be special in order to ensure that
+   * the puzzle is solvable.
+   * see:
+   * https://www.cs.princeton.edu/courses/archive/spring21/cos226/assignments/8puzzle/specification.php#:~:text=Thus%2C%20if%20a%20board%20has,inversions%2C%20then%20it%20is%20solvable.
+   * */
+  let currentIndex = array.length;
+
+  // First do a normal Fisher-Yates shuffle
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    swap(array, currentIndex, randomIndex);
+  }
+
+  if (isOdd(array.length)) {
+    // If the puzzle is odd numbered, inversions must be even
+    var inversions = countInversions(array);
+    if (isOdd(inversions)) {
+      removeFirstInversion(array);
+    }
+  }
+  else {
+    // If the puzzle is even numbered, inversions + row of the blank square must be odd
+    var inversions = countInversions(array);
+    if (isEven(inversions + divideInt(array.indexOf(null), numberOfCols))) {
+      inversions === 0 ? addOneInversion(array) : removeFirstInversion(array);
+    }
+  }
+
+  return array;
+}
+
+function countInversions(array: any[]) {
+  let inversions = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === null) continue;
+      if (array[i] === null) continue;
+
+      if (array[i] > array[j]) {
+        inversions++;
+      }
+    }
+  }
+
+  return inversions;
+}
+
+function removeFirstInversion(array: any[]) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === null) continue;
+      if (array[i] === null) continue;
+
+      if (array[i] > array[j]) {
+        swap(array, i, j);
+        return;
+      }
+    }
+  }
+}
+
+function addOneInversion(array: any[]) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === null) continue;
+      if (array[i] === null) continue;
+
+      if (array[i] < array[j]) {
+        swap(array, i, j);
+        return;
+      }
+    }
+  }
+}
+
+function isOdd(num: number) {
+  return num % 2 === 1;
+}
+
+function isEven(num: number) {
+  return num % 2 === 0;
+}
+
+export { divideInt, swap, getRandomColor, slidePuzzleShuffle, countInversions, removeFirstInversion, addOneInversion, isOdd, isEven };
