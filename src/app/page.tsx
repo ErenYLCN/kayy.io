ef1d8b80406cc827028c329c6d7e8dc8a10bcb0c
Eren Yalçın <yalcineren97@gmail.com>
2024-06-20 18:40:30 +0300

    feat(home): reduce padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import P5Wrapper from "@/component/P5Wrapper";
 
 export default function Home() {
   return (
-    <main className={"flex min-h-screen flex-col items-center p-24"}>
+    <main className={"flex min-h-screen flex-col items-center p-12"}>
       {"kay.io"}
 
       <P5Wrapper board={board} />
