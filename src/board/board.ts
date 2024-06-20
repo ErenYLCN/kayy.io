340c453c9f9cc59e819920010282ba9fc4ba3def
Eren Yalçın <yalcineren97@gmail.com>
2024-06-20 16:34:58 +0300

    feat(board): improve cell structure

--- src/board/board.ts
+++ src/board/board.ts
@@ -21,7 +21,20 @@ const board = (p: p5) => {
   /* create boardData array, an array with N = ROWS x ROWS cells, first N - 1 cells have 
    * value {id}, last cell has value null
    */
-  const boardData = [...Array.from({ length: TOTAL_CELLS - 1 }, (_, id) => id), null];
+  const boardData =
+    [
+      ...Array.from(
+        { length: TOTAL_CELLS - 1 },
+        (_, id) => (
+          {
+            id,
+            x: (id % COLS) * CELL_DIMENSION,
+            y: (divideInt(id, COLS)) * CELL_DIMENSION
+          }
+        )
+      ),
+      null
+    ];
   let nullIndex = TOTAL_CELLS - 1;
 
   p.setup = () => {
@@ -32,50 +45,58 @@ const board = (p: p5) => {
   p.draw = () => {
     p.background(200);
     p.fill(255, 0, 0);
-    boardData.forEach((id, index) => {
-      if (id === null) return;
+    boardData.forEach((cell, index) => {
+      if (cell === null) return;
       p.rect
         (
-          (index % COLS) * CELL_DIMENSION,
-          (divideInt(index, COLS)) * CELL_DIMENSION,
+          cell.x,
+          cell.y,
           CELL_DIMENSION, CELL_DIMENSION
         )
     });
   };
 
   p.keyPressed = () => {
+    console.log(boardData);
     switch (p.keyCode) {
       case p.LEFT_ARROW:
-        if (nullIndex % COLS !== COLS - 1) {
+        var cell = boardData[nullIndex + 1];
+        if (nullIndex % COLS !== COLS - 1 && cell !== null) {
           swap(boardData, nullIndex, nullIndex + 1);
           nullIndex += 1;
+          cell.x -= CELL_DIMENSION;
           p.clear();
         }
         break;
       case p.RIGHT_ARROW:
-        if (nullIndex % COLS !== 0) {
+        var cell = boardData[nullIndex - 1];
+        if (nullIndex % COLS !== 0 && cell !== null) {
           swap(boardData, nullIndex, nullIndex - 1);
           nullIndex -= 1;
+          cell.x += CELL_DIMENSION;
           p.clear();
         }
         break;
       case p.UP_ARROW:
-        if (divideInt(nullIndex, COLS) !== ROWS - 1) {
+        var cell = boardData[nullIndex + COLS];
+        if (divideInt(nullIndex, COLS) !== ROWS - 1 && cell !== null) {
           swap(boardData, nullIndex, nullIndex + COLS);
           nullIndex += COLS;
+          cell.y -= CELL_DIMENSION;
           p.clear();
         }
         break;
       case p.DOWN_ARROW:
-        if (divideInt(nullIndex, COLS) !== 0) {
+        var cell = boardData[nullIndex - COLS];
+        if (divideInt(nullIndex, COLS) !== 0 && cell !== null) {
           swap(boardData, nullIndex, nullIndex - COLS);
           nullIndex -= COLS;
+          cell.y += CELL_DIMENSION;
           p.clear();
         }
         break;
     }
   };
-
 };
 
 export default board;
